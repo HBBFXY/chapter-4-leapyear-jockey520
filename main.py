2464a0436790206afa381252da7f7008e4eac2ef
jockey520 <2251447784@qq.com>
2025-09-22 21:22:01 +0800

    闰年判断

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# 在此处编写代码
+try:
+    year = eval(input(""))
+    if year%4 ==0:
+        if year % 400 == 0:
+            print("是闰年")
+        elif year % 100 != 0:
+            print("是闰年")
+        else:
+            print("不是闰年")
+    else:
+        print("不是闰年")
+except :
+    print("输入错误，请输入一个整数") 在此处编写代码
